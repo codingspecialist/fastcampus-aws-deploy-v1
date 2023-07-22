1a6e7f199a5d76f711618b95a61bc55a64665d91
metacoding <getinthere@naver.com>
2023-07-22 15:08:32 +0900

    테스트 프ë¡프로파일 등록

---
+++ src/test/java/shop/mtcoding/deployapp/UserControllerTest.java
@@ -0,0 +1,9 @@
+package shop.mtcoding.deployapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UserControllerTest {
+}
